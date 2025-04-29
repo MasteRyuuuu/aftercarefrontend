aee0fd4a4f42107832f4708faabe280bf5fe7ab9
MasteRyuuuu <452474047@qq.com>
2025-04-28 23:50:03 -0700

    111

--- backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f74318a240d78d45460a881fc4058006557ac09d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00a87c52088da1474e3ebe316b7af9e0315c2680")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
